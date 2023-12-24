fff167587c705cf54d88621d5a5144f5a5df48ea
NDCodingGH <154352829+NDCodingGH@users.noreply.github.com>
2023-12-24 13:39:08 +0000

    update

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
+$(window).on('hashchange', function(e){ // listen if hashtag is being added to the URL
+
+  location.href = location.href.replace(location.hash,"") //replace it with nothing
+  console.log("bam!"); //enjoy it
+
+});
