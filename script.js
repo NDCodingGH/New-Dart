f0c903c416b2377bbdcb181c20c9664d56eb4ff9
NDCodingGH <154352829+NDCodingGH@users.noreply.github.com>
2023-12-24 13:58:10 +0000

    update

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
-// Check if the URL contains an anchor
-if (window.location.hash) {
-  // Remove the anchor from the URL without reloading the page
-  history.replaceState({}, document.title, window.location.pathname);
-}
