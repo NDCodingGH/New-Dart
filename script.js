75fa416cbff6563ca90b706c87fb597962401a40
NDCodingGH <154352829+NDCodingGH@users.noreply.github.com>
2023-12-25 21:48:00 +0000

    update lamayor & new dart

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+function copyEmailToClipboard(imgElement) {
+  var email = imgElement.getAttribute("data-email");
+  navigator.clipboard.writeText(email);
+  alert("Email copiado: " + email);
+}
